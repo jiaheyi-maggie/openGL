8bfc7ebfd08589d53ead6b52c431daf1874706d7
Maggie Yi <myi_gomboc@berkeley.edu>
2020-12-01 01:42:35 -0800

    init commit

---
+++ base.py
@@ -0,0 +1,33 @@
+import pygame
+from pygame.locals import *
+from OpenGL.GL import *
+from OpenGL.GLE import *
+
+vertices = (
+    (1, -1, -1), #1
+    (1, 1, -1),
+    (-1, 1, -1),
+    (-1, -1, -1),
+    (1, -1, 1),
+    (1, 1, 1),
+    (-1, 1, 1),
+    (-1, -1, 1),
+)
+
+edges = (
+    (0, ),
+    (0, ),
+    (0, ),
+    (),
+    (),
+    (),
+    (),
+    (),
+    (),
+    (),
+    (),
+    (),
+)
+
+
+
