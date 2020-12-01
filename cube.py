223289416e7792a18ff7c3513a5e46b9945ea09e
Maggie Yi <myi_gomboc@berkeley.edu>
2020-12-01 12:44:47 -0800

    interactivity

--- cube.py
+++ cube.py
@@ -77,14 +77,24 @@ def main():
     pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
 
     gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
-    glTranslatef(0.0, 0.0, -5)
-    glRotatef(40, 2, 0, 0)
+    glTranslatef(0.0, 0.0, -10)
+    glRotatef(25, 2, 1, 0)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    glTranslatef(-1, 0.0, 0.0)
+                if event.key == pygame.K_RIGHT:
+                    glTranslatef(1, 0.0, 0.0)
+                if event.key == pygame.K_UP:
+                    glTranslatef(0, 1, 0.0)
+                if event.key == pygame.K_DOWN:
+                    glTranslatef(0, -1, 0.0)
+
 
         glRotatef(1, 3, 1, 1)
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
